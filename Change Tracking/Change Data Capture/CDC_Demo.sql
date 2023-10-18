067da63aafbd089ffb58f7a6554562e0aedb23e6
Standa Zitta <stanislav.zitta@ronalgroup.com>
2023-10-18 07:52:39 +0200

    continued with CDC example

--- Change Tracking/Change Data Capture/CDC_Demo.sql
+++ Change Tracking/Change Data Capture/CDC_Demo.sql
@@ -16,3 +16,15 @@ the list of captured columns and retry the operation.
 
 */
 EXEC sp_cdc_enable_table @source_schema = 'dbo' , @source_name = 'cdc_demo', @captured_column_list = 'id,text' , @role_name = 'CDC'
+
+/* show me current config */
+EXEC sys.sp_cdc_help_change_data_capture
+
+DECLARE @from_lsn binary(10), @to_lsn binary(10)
+SET @from_lsn = sys.fn_cdc_get_min_lsn('SalesLT.address')
+SET @to_lsn = sys.fn_cdc_get_max_lsn()
+SELECT * FROM cdc.fn_get_all_changes_SalesLT_address(@from_lsn, @to_lsn, N'all');
+
+EXEC sp.cdc_disable_table  @source_schema = 'dbo' , @source_name = 'cdc_demo', @capture_instance = 'all' -- stop all CDC instances on the table
+-- disable for the whole database: 
+EXEC sp_cdc_disable_db;
