07b72eedc5d7d9094fca977a35d1e8cbfac31253
szi7443 <zittastanislav@hotmail.cz>
2024-05-21 15:42:52 +0200

    Added powershell script for querying of prepared list of SQL servers (Powershell queries OS version and SQL version+edition)

---
+++ Monitoring/SQL server itinerary exploration - source - Excel.ps1
@@ -0,0 +1,37 @@
+import-module importexcel
+import-module dbatools
+$excel = Import-Excel 'C:\MyWork\SqlServersList.xlsx'
+$Array = @()
+$Servers = @{}
+#$q= 0
+foreach($server in $excel) {
+    #$q++
+    $pk = Get-DbaProductKey -computername $server.'SQL Server'
+    <# instance count is not used anywhere, it's only for demonstration here #>
+    #$instance_count = ($pk | Measure-Object).Count
+    $os = Get-DBAOperatingsystem $server.'SQL Server'
+    $Instances = @()
+    $Versions = @()
+    $Editions = @()
+    foreach($i in $pk) {
+        $Instances+= $i.sqlinstance
+        $Versions += $i.version
+        $Editions += $i.Edition
+    }
+    $servername = $($server.'SQL Server').ToString()
+    $Servers.Add($servername,@($Instances,$Versions,$Editions,$os.OSVersion))
+
+   <# if($q -gt 2) {
+        break
+    }#>
+}
+
+write-host "-------------------"
+foreach($key in  $Servers.Keys) {
+    $m = 0
+    write-host $("ServerName:"+$key.ToString() + " " + "OS: " +$Servers[$key][3])
+    for($q=0; $q -lt $Servers[$key][0].Length;$q++) {
+        write-host $("Instance: " + $Servers[$key][0][$q].ToString() + "; Versions: " + $Servers[$key][1][$q].ToString() + "; Editions:" + $Servers[$key][2][$q].ToString()) 
+    }
+#    write-host $Servers[$key][3]
+}
