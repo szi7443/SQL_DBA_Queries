7eaafd7b58756805f857ba6a6ab9415f808d445c
szi7443 <zittastanislav@hotmail.cz>
2024-06-20 16:59:21 +0200

    Using .NET methods for SQL querying instead of invoke-sqlcmd

--- Monitoring/SQL server itinerary exploration - source - Excel.ps1
+++ Monitoring/SQL server itinerary exploration - source - Excel.ps1
@@ -1,3 +1,4 @@
+
 import-module importexcel
 import-module dbatools
 <# make sure to install SQL server module in powershell before running this script: 
@@ -5,7 +6,7 @@ import-module dbatools
 as per my testing, default version of the sqlserver module is broken, so: 
 install-module sqlserver -RequiredVersion 21.1.18256
  #>
-$excel = Import-Excel 'C:\MyWork\SQLServersList.xlsx'
+$excel = Import-Excel 'your_excelsheet.xlsx'
 $Array = @()
 $Servers = @{}
 #$q= 0
@@ -39,21 +40,49 @@ FROM sys.dm_os_sys_info
 GO
 "
 
+function Invoke-SQL {
+    param(
+        [string] $dataSource = ".\SQLEXPRESS",
+        [string] $database = "MasterData",
+        [string] $sqlCommand = $(throw "Please specify a query.")
+      )
+
+    $connectionString = "Data Source=$dataSource; " +
+            "Integrated Security=SSPI; " +
+            "Initial Catalog=$database"
+
+    $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+    $command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+    $connection.Open()
+    
+    $adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataSet) | Out-Null
+    
+    $connection.Close()
+    $dataSet.Tables
+
+}
+
+
 foreach($server in $excel) {
     #$q++
+
     <# instance count is not used anywhere, it's only for demonstration here #>
     #$instance_count = ($pk | Measure-Object).Count
-
     $instances = (Get-DbaProductKey -ComputerName  $server.'SQL Server' -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | select sqlinstance)
     $icount = ($instances | measure-object).Count
-    
     Write-Host $server.'SQL Server'
     if($icount -gt 0) {
         for($q = 0;$q -lt $icount;$q++ ) {
         $instances[$q].SqlInstance | Out-File DBInventory.txt -Append
         #Get-DbaInstanceProperty -sqlinstance $instances[$q].SqlInstance | Where-Object {$_.Name -like 'HostDistribution' -or $_.Name -like 'Processors' -or  $_.Name -like 'VersionMajor' -or   $_.Name -like 'Edition'} | select name, value, ComputerName, InstanceName | Format-Table -AutoSize
-        Invoke-Sqlcmd -serverinstance $instances[$q].SqlInstance -query $query -TrustServerCertificate | Format-Table | Out-File DBInventory.txt -Append
-     
+       
+        #write-host $instances[$q].SqlInstance.ToString()
+        #Invoke-Sqlcmd -serverinstance $instances[$q].SqlInstance -query $query -TrustServerCertificate | Format-Table | Out-File DBInventory.txt -Append
+        $output = Invoke-SQL -dataSource $instances[$q].SqlInstance.ToString() -sqlCommand $query -database "master"
+        
+        $output | Format-Table | Out-File DBInventory.txt -Append
         $rs = New-PSSession -ComputerName $server.'SQL Server'
         $remotelastexitcode = Invoke-Command  -Session $rs -ScriptBlock {
         $vCores = Get-WmiObject Win32_Processor | Measure -Property  NumberOfCores -Sum
@@ -66,10 +95,11 @@ foreach($server in $excel) {
        $enabled = 1
     } 
    else {  $HT=“Hyper Threading: Disabled”
+    $enabled = 0
     } 
     return $enabled        }
     "hyperthreading enabled --> "+$remotelastexitcode.ToString() | Out-File DBInventory.txt -Append
-
+    '-'*60 | Out-File DBInventory.txt -Append
     remove-pssession $rs
         }
     }
